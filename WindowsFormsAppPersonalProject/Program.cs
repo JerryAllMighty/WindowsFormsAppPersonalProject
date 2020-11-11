93170e5243834134bd9294ee57a6ff173221a6a7
정준호 <yks3858@gmail.com>
2020-11-11 09:37:40 +0900

    겟 체크

--- WindowsFormsAppPersonalProject/Program.cs
+++ WindowsFormsAppPersonalProject/Program.cs
@@ -7,7 +7,7 @@ using System.Windows.Forms;
 namespace WindowsFormsAppPersonalProject
 {
     static class Program
-    {
+    {  
         /// <summary>
         /// 해당 애플리케이션의 주 진입점입니다.
         /// </summary>
